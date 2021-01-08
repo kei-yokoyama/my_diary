c478c6b029387b466469f715fbaf821fe89531ae
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2021-01-08 19:21:53 +0900

    投稿機能モデル単体テスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :post do
-    
+    title                 { "今日の日記" }
+    text                  { "よろしくお願いします。" }
+    association :user
+
+    #after(:build) do |item|
+      #item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    #end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Post do
+    before do
+      @post = FactoryBot.build(:post)
+    end
+
+    describe '日記投稿機能' do
+      context '投稿がうまくいくとき' do
+        it 'ログイン状態で、タイトル、本文、が存在すれば投稿の保存が可能' do
+          expect(@post).to be_valid
+        end
+      end
+
+      context '投稿がうまくいかないとき' do
+        it 'userが紐付いていないと保存できないこと' do
+          @post.user = nil
+          @post.valid?
+          expect(@post.errors.full_messages).to include('User must exist')
+        end
+
+        it 'タイトルが空では保存できないこと' do
+          @post.title = nil
+          @post.valid?
+          expect(@post.errors.full_messages).to include("Title can't be blank")
+        end
+
+        it '本文が空では保存できないこと' do
+          @post.text = nil
+          @post.valid?
+          expect(@post.errors.full_messages).to include("Text can't be blank")
+        end
+      end
+    end
+  end
 end
