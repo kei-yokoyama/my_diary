705228a53a4bb9b7343e5cbd03ec5108ad0aa4c8
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2021-01-29 00:47:28 +0900

    絵文字テキスト入力バグ修正。
    絵文字パッドを閉じて再度開くと、２回絵文字が入力されてしまうバグの修正。

--- app/javascript/emoji_picker_title.js
+++ app/javascript/emoji_picker_title.js
@@ -2,7 +2,7 @@ function hover() {
   
   const emojiTitle = document.getElementById('emoji-title')
   const emojiPickerTitle = document.getElementById('emoji-picker-title')
-  var a = 0
+  var a = 0 //絵文字を１回だけクリックしても複数絵文字入力されるバグ修正。pickerを開くたびに'emoji-click'回数が増えるので起こる。
 
   emojiTitle.addEventListener('click', function () {
     if (emojiPickerTitle.getAttribute("style") == "display:block;") {
@@ -13,7 +13,7 @@ function hover() {
       emojiPickerTitle.setAttribute("style", "display:block;")
       emojiPickerTitle.addEventListener('emoji-click', function () {
         if (a == 0){
-        document.getElementById("title").value += event.detail.unicode;
+          document.getElementById("title").value += event.detail.unicode;
           a += 1
           emojiPickerTitle.removeAttribute("style", "display:block;")
         }
