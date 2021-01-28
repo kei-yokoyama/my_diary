436b99b68fa4f364802f7f17c39472654845f4a4
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2021-01-28 21:59:34 +0900

    絵文字タイトル入力バグ修正。
    絵文字パッドを閉じて再度開くと、２回絵文字が入力されてしまうバグの修正。

--- app/javascript/emoji_picker_title.js
+++ app/javascript/emoji_picker_title.js
@@ -2,17 +2,26 @@ function hover() {
   
   const emojiTitle = document.getElementById('emoji-title')
   const emojiPickerTitle = document.getElementById('emoji-picker-title')
+  var a = 0
 
   emojiTitle.addEventListener('click', function () {
     if (emojiPickerTitle.getAttribute("style") == "display:block;") {
       emojiPickerTitle.removeAttribute("style", "display:block;")
+      a = 0
     } else {
+      a = 0
       emojiPickerTitle.setAttribute("style", "display:block;")
       emojiPickerTitle.addEventListener('emoji-click', function () {
+        if (a == 0){
         document.getElementById("title").value += event.detail.unicode;
+          a += 1
+          emojiPickerTitle.removeAttribute("style", "display:block;")
+        }
       })
     }
   });
 };
+
+
 
 window.addEventListener('load', hover);
