ac33001604a6c796c09312ce1506b654008b8b92
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2021-02-04 03:02:59 +0900

    % rails g devise:controllers users
    omniauthコントローラー生成のため

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def google_oauth2
+    authorization
+  end
+  
+  private
+  
+  def authorization
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+  end
+  
+end
